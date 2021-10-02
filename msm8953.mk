76a51b81bd78e9a41d5b097a3e332f78f2cfc1a2
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2021-10-02 20:17:08 +0700

    msm8953-common: drop packages
    Signed-off-by: BayerischeMotorenWerke <syaifulamri199@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -279,10 +279,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
-# Remove unwanted packages
-PRODUCT_PACKAGES += \
-    Remove_Packages
-
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
