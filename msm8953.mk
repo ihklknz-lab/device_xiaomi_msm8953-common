96aca18c022ccec1ec5b4c2bd3ba0398b1a7bf2d
hklknz <haikalluthfianinobaluqia598@gmail.com>
2023-04-23 07:03:34 +0000

    msm8953-common: drop android.hidl.manager@1.0
    Signed-off-by: hklknz <haikalluthfianinobaluqia598@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -172,9 +172,6 @@ endif
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
-    android.hidl.manager@1.0
-
-PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
